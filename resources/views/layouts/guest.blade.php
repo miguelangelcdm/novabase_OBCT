d261196cf4c3e4e52e4c552e9ae912b1e4ce113a
miguelangel <miguelangelcdm@gmail.com>
2022-09-18 14:36:05 -0500

    listo para empezar a trabajar en el dashboard

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="theme-2">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,9 +13,5 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="login">
-        <div>
-            {{ $slot }}
-        </div>
-    </body>
+    {{ $slot }}
 </html>
