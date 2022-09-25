fd01db26d8f7929512db01f2fc128ede6ef9a4b2
miguelangel <miguelangelcdm@gmail.com>
2022-09-25 14:08:00 -0500

    chambeando el main dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,69 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque sapiente aperiam nam odit laborum
+                    aspernatur ad suscipit quidem? Dolore omnis ipsam velit, doloribus quibusdam suscipit hic tenetur.
+                    Distinctio, laudantium dignissimos.
+                    Accusantium, cumque. Modi laudantium, eum quae, nemo minima culpa, vitae qui dolores minus
+                    voluptatibus reiciendis. Hic natus eveniet labore consectetur officia suscipit. Magnam, totam
+                    similique quis sint hic autem! Nulla!
+                    Rem atque doloremque excepturi mollitia libero molestias saepe exercitationem officia quae! Dolor
+                    cupiditate perspiciatis odio earum dignissimos, at suscipit voluptate, aliquid alias, tempore
+                    laborum obcaecati maxime fugit. Eveniet, quasi sed.
+                    Consectetur natus, aut eos iste nemo numquam atque error voluptas autem harum quod reprehenderit
+                    totam in veniam sequi perferendis ex aliquam impedit tempore, deserunt incidunt. Blanditiis quaerat
+                    unde laboriosam velit?
+                    Nostrum dicta nemo facere deleniti consequuntur magnam minima delectus vel pariatur, eaque, neque,
+                    et temporibus soluta illo eius ab? Nam enim corporis beatae illum, odit maxime earum quod quae
+                    nostrum.
+                    Doloribus maiores fugiat voluptate. Quibusdam repellat quaerat expedita nobis, sequi maxime eius
+                    ipsa consequatur corporis nulla, quos maiores saepe dolorem blanditiis? Officiis quo animi quasi
+                    laudantium. Distinctio tempore corporis eos!
+                    Repudiandae cumque laboriosam fugiat asperiores dolore ratione praesentium cupiditate, neque ipsam
+                    voluptate veniam nemo ullam debitis voluptates quae magni, deserunt id pariatur libero. Repellendus,
+                    nobis voluptas tenetur iste totam quisquam!
+                    Neque a harum ipsum accusantium obcaecati, corrupti dolor incidunt deserunt quod placeat ipsam
+                    dolorem consequuntur ab, quis cupiditate modi dicta iusto, maxime laborum ducimus similique
+                    inventore odio quasi? Id, itaque.
+                    Perspiciatis ex quae provident, libero voluptatibus ea, quis natus doloribus, quos magnam quam fuga
+                    porro delectus assumenda eum vitae reprehenderit dolorum tempore suscipit! Tempore cumque rem
+                    reprehenderit dolore aliquid libero.
+                    Earum, pariatur ea deleniti mollitia iusto corrupti porro repellendus sunt, quia, quod debitis
+                    tempora deserunt explicabo ad consequatur molestiae omnis amet hic. Nisi voluptatem voluptates
+                    veniam alias accusamus officiis facilis.
+                    Labore ipsum quo fuga asperiores illo itaque! Earum tempora voluptatem obcaecati beatae suscipit,
+                    delectus corrupti fuga ipsum pariatur accusantium a reiciendis atque, magnam eum laborum aperiam
+                    rerum, inventore esse saepe!
+                    Ducimus consequatur, dicta ipsam debitis est, veniam ipsa incidunt velit et esse aliquid sed
+                    doloremque, quam odit nulla. Neque voluptate eveniet id. Necessitatibus ea impedit dolorum enim quo
+                    eligendi non?
+                    Consectetur expedita ducimus, dolor laudantium illo quis, maxime nostrum amet facilis doloribus
+                    laboriosam, dignissimos voluptatem ullam aperiam unde natus sit! Amet architecto tempora rem
+                    mollitia, soluta quae neque blanditiis tempore!
+                    Voluptatibus eligendi veniam, nisi ipsum ut architecto, dolorum culpa vitae tempore animi maxime
+                    sint ad laborum aut inventore reprehenderit. Ea, accusantium quisquam? Quam, voluptatem amet
+                    deserunt iusto enim sed commodi!
+                    Suscipit eum vel fugit explicabo earum harum exercitationem rem assumenda? Perferendis atque
+                    officiis optio iusto. Voluptates et rerum incidunt, necessitatibus molestiae ipsum pariatur dicta
+                    alias quod delectus deleniti, asperiores fugit.
+                    Eos porro fuga dolores laboriosam illo obcaecati error veniam sed, quaerat voluptates, cum deleniti,
+                    sint ut! Labore maiores nihil optio repudiandae aspernatur nam atque consequatur explicabo, ipsum
+                    odio harum iste!
+                    Est laudantium harum enim rerum rem expedita deserunt molestiae, natus cupiditate, reprehenderit
+                    molestias sed blanditiis quo ex adipisci, architecto iste! Et, expedita quod ut nisi tempora
+                    possimus aspernatur praesentium quibusdam!
+                    Nesciunt enim cumque, dicta expedita perspiciatis tenetur nobis totam dolor autem aut voluptates
+                    corporis rem quae a repudiandae iure accusantium saepe laboriosam et nisi ad aspernatur? Neque fuga
+                    iste optio.
+                    Voluptatem rerum doloribus quisquam illum iusto porro velit deleniti saepe nam ex, eaque culpa nemo,
+                    tempora, officia mollitia dolorum nulla aut possimus. Sint facere ullam aspernatur perspiciatis
+                    esse! Id, dolore!
+                    Omnis alias dolores labore quis, vitae saepe ipsam voluptatibus, cum tempore necessitatibus
+                    praesentium explicabo, molestias eaque rerum iusto tempora aperiam vel atque corporis? Labore
+                    sapiente beatae nisi, iusto debitis sunt.
+                </div>
+
             </div>
         </div>
     </div>
